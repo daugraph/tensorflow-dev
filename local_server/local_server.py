48d92e086bc4772b3908c8af6fd62f4c0cc17cbd
daugraph <daugraph@gmail.com>
2023-01-28 18:09:41 +0800

    Update workflow

--- local_server/local_server.py
+++ local_server/local_server.py
@@ -4,6 +4,8 @@ from flask import Flask, request, jsonify, send_from_directory, abort
 import os
 from flask import make_response
 
+os.chdir('.')
+
 app = Flask(__name__)
 
 @app.route("/<filename>", methods=['GET'])
